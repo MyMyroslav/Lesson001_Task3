fc14f26b88be34d08e1a2c65743313fa3de94951
Myroslav <havrushkomyroslav@gmail.com>
2020-10-22 20:59:47 +0300

    Done Lesson001_Task3

---
+++ Lesson001_Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson001_Task3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            decimal dMoney = 100;
+            Console.WriteLine(dMoney);
+        }
+    }
+}
